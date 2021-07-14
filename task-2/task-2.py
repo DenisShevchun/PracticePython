04eac2972ee9ed764b812d3e908359d1d78bffcf
Shevchun Denis <fellowastronomer14@gmail.com>
2021-07-14 16:58:11 +0300

    modified calculator

--- task-2/task-2.py
+++ task-2/task-2.py
@@ -1,3 +1,5 @@
+import math
+
 while True:
   print("\n-----------------HINT: You can type 'end' anytime to exit------------------\n")
   yourInput = input("Enter an integer number: ")
@@ -10,10 +12,27 @@ while True:
   else:
     num_1 = int(yourInput.strip())
   
-  operation = input("Enter an operation (+, -, *, /): ")
+  operation = input("Enter an operation (+, -, *, /, %, cos, sin, tan, ctan, log, ln): ")
   if operation == "end":
       break
-  operation = operation.strip()
+  elif operation == "cos":
+    print (math.cos(num_1))
+    continue
+  elif operation == "sin":
+    print (math.sin(num_1))
+    continue
+  elif operation == "tan":
+    print (math.tan(num_1))
+    continue
+  elif operation == "ctan":
+    print (math.cos(num_1)/math.sin(num_1))
+    continue
+  elif operation == "log":
+    print (math.log(num_1))
+    continue
+  elif operation == "ln":
+    print (math.log1p(num_1))
+    continue
   
   anotherInput = input("Enter another number: ")
   if anotherInput == "end":
@@ -33,6 +52,8 @@ while True:
     ans = num_1*num_2
   elif operation == "/":
     ans = num_1/num_2
+  elif operation == "%":
+    ans = num_1%num_2
   else:
     print("invalid operation.")
     continue
